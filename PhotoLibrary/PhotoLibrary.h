2531300d382b06d6975813f605e5f3a449d367dd
Nikita Benin <nikitab22@me.com>
2020-06-20 19:22:56 +0200

    Initial Commit

---
+++ PhotoLibrary/PhotoLibrary.h
@@ -0,0 +1,19 @@
+//
+//  PhotoLibrary.h
+//  PhotoLibrary
+//
+//  Created by Nikita Benin on 20.06.2020.
+//  Copyright © 2020 Nikita Benin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhotoLibrary.
+FOUNDATION_EXPORT double PhotoLibraryVersionNumber;
+
+//! Project version string for PhotoLibrary.
+FOUNDATION_EXPORT const unsigned char PhotoLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoLibrary/PublicHeader.h>
+
+
